b64bee680dec981eca4c64ed46bb2a4b61f89a5d
Krista Nielsen <kristanielsen@Kristas-MacBook-Air.local>
2021-09-10 12:47:48 -0700

    producttag and tag.js files

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -16,20 +16,14 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-  as: 'product_producttags'
+  through: ProductTag,
+  foreignKey: 'product_id'
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-  as: 'tag_producttags'
+  through: ProductTag,
+  foreignKey: 'tag_id'
 });
 
 module.exports = {
