6d10c9c7d8cb7134ca9d2fbcaca850198c04a05b
Krista Nielsen <kristanielsen@Kristas-MacBook-Air.local>
2021-09-10 11:22:02 -0700

    setting up product.js file

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -1,5 +1,6 @@
 // import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
+const { Category } = require('.');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -9,7 +10,43 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      //decimal(precision, scale) to set as 2 decimal places
+      type: DataTypes.DECIMAL(10,2),
+      allowNull: false,
+      //validate that value is decimal 
+      validate: { 
+        isDecimal:true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      //validate that is numeric
+      validate: {
+        isInt: true
+      },
+      //setting value as 10 for default
+      defaultValue: 10
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
